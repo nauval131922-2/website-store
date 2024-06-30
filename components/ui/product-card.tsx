5c25c5a91ab07fd3d3339c24b3dfa1ad0a28c281
Nauval Gunawan <nauvalgunawan5@gmail.com>
2024-06-30 15:33:59 +0700

    update progress

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,35 +1,53 @@
-'use client'
+"use client";
 
-import { Product } from "@/types"
-import Image from "next/image"
-import IconButton from "./icon-button"
+import { Product } from "@/types";
+import Image from "next/image";
+import IconButton from "./icon-button";
+import { Expand } from "lucide-react";
+import Currency from "./currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCardProps {
-    data: Product
+  data: Product;
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({
-    data
-}) => {
-    return (
-        <div className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
-            {/* Images dan action */}
-            <div className="aspect-square rounded-xl bg-gray-100 relative">
-                <Image
-                alt="product image"
-                src={data.images?.[0]?.url}
-                fill
-                className="aspect-square rounded-md object-cover"
-                />
-                <div className="opacity-0 group-hover:opacity-100">
-                    <div className="flex gap-x-6 justify-center">
-                        <IconButton 
-                        icon={<Expand />} />
-                    </div>
-                </div>
-            </div>
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`/product/${data?.id}`);
+  };
+
+  return (
+    <div onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+      {/* Images dan action */}
+      <div className="aspect-square rounded-xl bg-gray-100 relative">
+        <Image
+          alt="product image"
+          src={data.images?.[0]?.url}
+          fill
+          className="aspect-square rounded-md object-cover"
+        />
+        <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
+          <div className="flex gap-x-6 justify-center">
+            <IconButton
+              icon={<Expand size={20} className="text-gray-600" />}
+              onClick={() => {}}
+            />
+          </div>
         </div>
-    )
-}
+      </div>
+      {/* Product Description */}
+      <div>
+        <p className="font-semibold text-large">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      {/* Product Price */}
+      <div className="flex items-center justify-between">
+        <Currency value={data?.price} />
+      </div>
+    </div>
+  );
+};
 
-export default ProductCard
+export default ProductCard;
